abdc6569386a163bb85e9ed0e5374667e2508975
Jannis R <mail@jannisr.de>
2017-02-16 00:58:06 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 'use strict'
 
 const subscription = require('./lib/subscription')
-const once = require('./lib/once')
-const blockByNr = require('./lib/block-by-nr')
-const coinbase = require('./lib/coinbase')
-const hashrate = require('./lib/hashrate')
-const mining = require('./lib/mining')
+const nullary = require('./lib/nullary')
+const unary = require('./lib/unary')
+
+const blockByNr = (api, nr) =>
+  api.eth.getBlockByNumber(nr)
+
+const coinbase = (api) =>
+  api.eth.coinbase()
+  .then((base) => base)
+
+const hashrate = (api) =>
+  api.eth.hashrate()
+  .then((rate) => +rate)
+
+const gasPrice = (api) =>
+  api.eth.gasPrice()
+  .then((price) => +price)
+
+const mining = (api) =>
+  api.eth.mining()
+
+const protocolVersion = (api) =>
+  api.eth.protocolVersion()
+  .then((version) => +version)
+
+const syncing = (api) =>
+  api.eth.syncing()
 
 module.exports = {
-  blockByNr,
-  coinbase,
-  currentBlock: subscription('eth_blockNumber'),
+  blockByNr: unary(blockByNr),
+  coinbase: nullary(coinbase),
+  currentBlock: subscription('eth_blockNumber', (v) => +v),
   defaultAccount: subscription('parity_defaultAccount'),
-  hashrate,
-  mining,
-  protocolVersion: once((api) => api.eth.protocolVersion())
+  gasPrice: nullary(gasPrice),
+  hashrate: nullary(hashrate),
+  mining: nullary(mining),
+  protocolVersion: nullary(protocolVersion),
+  syncing: nullary(syncing)
 }
