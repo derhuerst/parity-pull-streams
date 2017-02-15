0dbf0feaed63fea411105b405d47cdc1c46a7cce
Jannis R <mail@jannisr.de>
2017-02-16 00:28:29 +0100

    hashrate, mining, protocolVersion

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict'
 
 const subscription = require('./lib/subscription')
+const once = require('./lib/once')
 const blockByNr = require('./lib/block-by-nr')
 const coinbase = require('./lib/coinbase')
+const hashrate = require('./lib/hashrate')
+const mining = require('./lib/mining')
 
 module.exports = {
-  currentBlock: subscription('eth_blockNumber'),
   blockByNr,
-  coinbase
+  coinbase,
+  currentBlock: subscription('eth_blockNumber'),
+  defaultAccount: subscription('parity_defaultAccount'),
+  hashrate,
+  mining,
+  protocolVersion: once((api) => api.eth.protocolVersion())
 }
