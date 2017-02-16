70e8539c0127af636e5d06fdff7d025d1a922f7b
Jannis R <mail@jannisr.de>
2017-02-16 01:38:53 +0100

    more nullary & unary RPCs

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const subscription = require('./lib/subscription')
 const nullary = require('./lib/nullary')
 const unary = require('./lib/unary')
 
+const accounts = (api) =>
+  api.eth.accounts()
+
+const balance = (api, account) =>
+  api.eth.getBalance(account)
+  .then((count) => +count)
+
 const blockByNr = (api, nr) =>
   api.eth.getBlockByNumber(nr)
+
+const chain = (api) =>
+  api.parity.netChain()
 
 const coinbase = (api) =>
   api.eth.coinbase()
@@ -19,24 +29,50 @@ const gasPrice = (api) =>
   api.eth.gasPrice()
   .then((price) => +price)
 
+const listening = (api) =>
+  api.net.listening()
+
 const mining = (api) =>
   api.eth.mining()
+
+const nonce = (api, account) =>
+  api.parity.nextNonce(account)
+  .then((nonce) => +nonce)
+
+const peerCount = (api) =>
+  api.net.peerCount()
+  .then((count) => +count)
 
 const protocolVersion = (api) =>
   api.eth.protocolVersion()
   .then((version) => +version)
 
+const registry = (api) =>
+  api.parity.registryAddress()
+
 const syncing = (api) =>
   api.eth.syncing()
 
+const transactionCount = (api, account) =>
+  api.eth.getTransactionCount(account)
+  .then((count) => +count)
+
 module.exports = {
+  accounts: nullary(accounts),
+  balance: unary(balance),
   blockByNr: unary(blockByNr),
+  chain: nullary(chain),
   coinbase: nullary(coinbase),
   currentBlock: subscription('eth_blockNumber', (v) => +v),
   defaultAccount: subscription('parity_defaultAccount'),
   gasPrice: nullary(gasPrice),
   hashrate: nullary(hashrate),
+  listening: nullary(listening),
   mining: nullary(mining),
+  nonce: unary(nonce),
+  peerCount: nullary(peerCount),
   protocolVersion: nullary(protocolVersion),
-  syncing: nullary(syncing)
+  registry: nullary(registry),
+  syncing: nullary(syncing),
+  transactionCount: unary(transactionCount)
 }
